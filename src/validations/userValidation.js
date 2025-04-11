d9b27793f51e0759353afd1ae86d54c04bbaf4f9
batumzff <batuhanmuzafferoglu@gmail.com>
2025-04-11 22:36:55 +0300

    update user validation schema

--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -1,13 +1,13 @@
 const Joi = require("joi");
 
 module.exports.registerSchema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
+    username: Joi.string().min(3).max(30).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).max(30).required(),
-    phone: Joi.string().min(10).max(15).required(),
+    userType: Joi.string().min(3).max(15).required(),
     });
 
 module.exports.loginSchema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
+    username: Joi.string().min(3).max(30).required(),
     password: Joi.string().min(6).max(30).required(),
     });
